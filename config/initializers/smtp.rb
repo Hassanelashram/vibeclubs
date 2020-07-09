9b296390eacde9111af2b49b19634dbd1c623f78
Hassanelashram <elashramhassan@gmail.com>
2020-07-10 00:46:51 +0200

    d

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
   domain: 'gmail.com',
-  user_name: ENV['brastodesigns@gmail.com'],
-  password: ENV['letsgetrich'],
+  user_name: 'brastodesigns@gmail.com',
+  password: 'letsgetrich',
   authentication: :login,
   enable_starttls_auto: true
 }
