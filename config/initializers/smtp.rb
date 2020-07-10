42a11181593f4a3d5c844d3b3fc870e765fab459
Hassanelashram <elashramhassan@gmail.com>
2020-07-10 11:10:05 +0200

    d

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: 'gmail.com',
-  user_name: 'brastodesigns@gmail.com',
-  password: 'letsgetrich',
-  authentication: :login,
+  address:              'smtp.postmarkapp.com',
+  port:                 587,
+  domain:               'vibeclubs.herokuapp.com',
+  user_name:            'c1fae8ea-9a9d-4193-aa07-a1cf73f9e280',
+  password:             'c1fae8ea-9a9d-4193-aa07-a1cf73f9e280',
+  authentication:       :plain,
   enable_starttls_auto: true
 }
